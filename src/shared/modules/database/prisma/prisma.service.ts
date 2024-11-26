bfbcdf001ab5d8da5a836f7dfb300b7fd909269b
Victor Sousa <victor.henriqueoff@gmail.com>
2024-11-26 10:55:43 -0300

    feat: adding script to automatically create drivers when powering on the server

--- src/shared/modules/database/prisma/prisma.service.ts
+++ src/shared/modules/database/prisma/prisma.service.ts
@@ -1,9 +1,37 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+import { mockDrivers } from "src/modules/driver/mock/drivers";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+
+      const drivers = await this.drivers.findMany();
+
+      if (drivers.length === 0) {
+        mockDrivers.map(async (data) => {
+          await this.drivers.create({
+            data: {
+              name: data.name,
+              description: data.description,
+              vehicle: data.vehicle,
+              min_km_fee: data.min_km_fee,
+              min_trip_km: data.min_trip_km,
+              Review: {
+                create: {
+                  rating: data.rating,
+                  comment: data.comment,
+                },
+              },
+            },
+          });
+        });
+        console.log("Motoristas adicionados automaticamente no banco");
+      }
+    } catch (error) {
+      console.error("Erro ao inicializar dados do Prisma:", error);
+    }
   }
 }
